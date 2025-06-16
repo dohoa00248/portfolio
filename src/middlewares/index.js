5cf27d9b4aaf8dbf708d13b6642c6f1142121d55
dohoa00248 <dohoa00248@gmail.com>
2025-06-16 11:47:59 +0700

    feat(session): integrate MongoStore for production sessions

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,22 +1,30 @@
 import express from 'express';
 import session from 'express-session';
 import methodOverride from 'method-override';
+import MongoStore from 'connect-mongo';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const applyMiddleware = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: process.env.DB_MONGODB_URI,
+        ttl: 60 * 60,
+      }),
       cookie: {
         httpOnly: true,
         maxAge: 1000 * 60 * 60,
       },
     })
   );
+
   app.use(methodOverride('_method'));
 };
 
