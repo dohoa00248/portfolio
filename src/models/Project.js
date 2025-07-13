e028c6465c8e2f6b991c53bc35ac3e6abbcad958
dohoa00248 <dohoa00248@gmail.com>
2025-07-13 12:06:28 +0700

    fix model Project

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,26 +1,31 @@
 import mongoose from 'mongoose';
 
-const projectSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    tech: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    live: {
+      type: String,
+    },
+    github: {
+      type: String,
+      required: true,
+    },
   },
-  tech: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  live: {
-    type: String,
-  },
-  github: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Project = mongoose.model('Project', projectSchema);
 
